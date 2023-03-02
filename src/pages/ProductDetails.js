2b616c2600c1124780bbc81e77b3ab80ffdd07ab
JR <jasonrouss@gmail.com>
2023-03-02 23:03:30 +0200

    changes width

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -30,11 +30,11 @@ const ProductDetails = () => {
               <img
                 src={img}
                 alt={name}
-                className="lg:h-[250px] h-[190px] lg:w-[230px]  w-[150px] rounded-l-xl  border-l-2 border-y-2    border-indigo-500"
+                className="lg:h-[250px] h-[190px] lg:w-[230px]  w-[130px] rounded-l-xl  border-l-2 border-y-2    border-indigo-500"
               />
             </div>
             {/* Product Info */}
-            <div className="card-info pl-3 bg-indigo-100 rounded-r-xl lg:w-[230px] w-[150px] h-[190px] lg:h-[250px] grid  content-between">
+            <div className="card-info pl-3 bg-indigo-100 rounded-r-xl lg:w-[230px] w-[130px] h-[190px] lg:h-[250px] grid  content-between">
               <div className="text-[26px] font-bold">{name}</div>
               <div className="italic font-semibold">{description}</div>
               <div className="price add-to-cart flex justify-between p-2">
@@ -56,34 +56,34 @@ const ProductDetails = () => {
           <div className="my-10  lg:mx-4">
             <div>
               <div className="flex    ">
-                <div className="border-2 rounded-t-lg rounded-r-none border-indigo-500  lg:w-[230px] w-[150px] px-4 py-2">
+                <div className="border-2 rounded-t-lg rounded-r-none border-indigo-500  lg:w-[230px] w-[130px] px-4 py-2">
                   Weight:
                 </div>
-                <div className="border-r-2 border-t-2 border-b-2 rounded-l-none   rounded-t-lg border-indigo-500    lg:w-[230px] w-[150px] px-4 py-2">
+                <div className="border-r-2 border-t-2 border-b-2 rounded-l-none   rounded-t-lg border-indigo-500    lg:w-[230px] w-[130px] px-4 py-2">
                   {weight}
                 </div>
               </div>
               <div className="flex">
-                <div className="border-x-2 border-b-2 border-indigo-500   lg:w-[230px] w-[150px] px-4 py-2">
+                <div className="border-x-2 border-b-2 border-indigo-500   lg:w-[230px] w-[130px] px-4 py-2">
                   Dimension:
                 </div>
-                <div className="border-r-2 border-b-2 border-indigo-500 lg:w-[230px] w-[150px] px-4 py-2">
+                <div className="border-r-2 border-b-2 border-indigo-500 lg:w-[230px] w-[130px] px-4 py-2">
                   {dimension}
                 </div>
               </div>
               <div className="flex">
-                <div className="border-x-2  border-indigo-500   lg:w-[230px] w-[150px] px-4 py-2">
+                <div className="border-x-2  border-indigo-500   lg:w-[230px] w-[130px] px-4 py-2">
                   Sold By:
                 </div>
-                <div className="border-r-2  border-indigo-500 lg:w-[230px] w-[150px] px-4 py-2">
+                <div className="border-r-2  border-indigo-500 lg:w-[230px] w-[130px] px-4 py-2">
                   {soldBy}
                 </div>
               </div>
               <div className="flex">
-                <div className="border-2 border-indigo-500  rounded-l-lg rounded-t-none  lg:w-[230px] w-[150px] px-4 py-2">
+                <div className="border-2 border-indigo-500  rounded-l-lg rounded-t-none  lg:w-[230px] w-[130px] px-4 py-2">
                   Rating:
                 </div>
-                <div className="border-r-2 border-y-2 border-indigo-500 rounded-r-lg  rounded-t-none flex  lg:w-[230px] w-[150px] px-4 py-2">
+                <div className="border-r-2 border-y-2 border-indigo-500 rounded-r-lg  rounded-t-none flex  lg:w-[230px] w-[130px] px-4 py-2">
                   {rating} <AiOutlineStar className="mt-1.5 ml-1" />
                 </div>
               </div>
